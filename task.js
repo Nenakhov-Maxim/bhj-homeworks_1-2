24c48fc652c2f351edac2fc12cb319cb441648be
Nenakhov.Max@yandex.ru <Nenakhov.Max@yandex.ru>
2021-01-07 12:17:41 +0500

    update

--- task.js
+++ task.js
@@ -5,12 +5,10 @@ function clickerTimer() {
     let startClickTimer = parseInt(document.getElementById("clicker__counter").textContent, 10);
     let newTimer = startClickTimer + 1;
     document.getElementById("clicker__counter").textContent = newTimer;
-    const image = document.getElementById("cookie");
-    if (image.width <= 200) {
-        image.width = 300;
+    if (timer.width <= 200) {
+        timer.width = 300;
     } else {
-        image.width = 200;
+        timer.width = 200;
     }
-
 }
 timer.onclick = clickerTimer;
